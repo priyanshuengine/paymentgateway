432df0692acb6539cb904826c5d942c85d3008dd
Priyanshu Chaturvedi <chaturvedi.priyanshu11@gmail.com>
2022-10-13 21:08:07 +0530

    pay commit

--- main.js
+++ main.js
@@ -16,24 +16,7 @@ $(document).ready(function(){
             }   
     });
 
-    var typed = new Typed(".typing-1",{
-        strings: [
-            "YouTuber", "Developer", "Blogger",
-            "Designer", "Freelancer"
-        ],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-2",{
-        strings: [
-            "YouTuber", "Developer", "Blogger",
-            "Designer", "Freelancer"
-        ],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    
 
     //slide-up script
     $('.scroll-up-btn').click(function(){
